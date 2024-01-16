f1b9466869779f5378a6ea81a4d524fa57bf7617
toshi0907 <n00toshi@gmail.com>
2024-01-16 09:55:28 +0900

    setup.sh の更新

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+DISP_ARG_OK="[\033[1;32m  OK  \033[0;39m]"
+DISP_ARG_NG="[\033[1;31mFailed\033[0;39m]"
+
+######################################################
+
 function InstallPackageByPackMng() {
   # $1 : Package manager name
   # $2 : Software name
@@ -12,13 +17,50 @@ function InstallPackageByPackMng() {
   echo -e "\n\n>>> $2 start" >>~/setuplog.txt
   sudo $1 install -y $2 &>>~/setuplog.txt
   if [ $? -ne 0 ]; then
-    echo -e -n "\033[1;31m"
-    echo "[Failed]"
-    echo -e -n "\033[0;39m"
+    echo -e "${DISP_ARG_NG}"
   else
-    echo -e -n "\033[1;32m"
-    echo "[  OK  ]"
-    echo -e -n "\033[0;39m"
+    echo -e "${DISP_ARG_OK}"
+  fi
+}
+
+function CommandCheck() {
+  local COMMAND_CHECK_CMDNAME=$1
+  local COMMAND_CHECK_CMDNAME_DSP="$(printf "%15s" "${COMMAND_CHECK_CMDNAME}")"
+  echo -e -n "         : ${COMMAND_CHECK_CMDNAME_DSP}\r"
+  which ${COMMAND_CHECK_CMDNAME} &>/dev/null
+  if [ $? -ne 0 ]; then
+    echo -e -n "${DISP_ARG_NG} : ${COMMAND_CHECK_CMDNAME_DSP} : === N/A ==="
+  else
+    echo -e -n "${DISP_ARG_OK} : ${COMMAND_CHECK_CMDNAME_DSP} : $(which ${COMMAND_CHECK_CMDNAME})"
+  fi
+  echo
+}
+
+function CreateSymLinkAndCheck() {
+  # $1 <ex> /base/file/path/filename
+  local lBaseFilePath=$1
+
+  # $2 <ex> /target/dir/path/
+  local lTargetDirPath=$2
+
+  # $3 <ex> target_file_name
+  local lTargetFileName=$3
+
+  if [ "$lTargetFileName" == "./" ]; then
+    lTargetFileName=${lBaseFilePath##*/}
+  fi
+
+  CheckAndCdDir ${lTargetDirPath}
+  ln -s ${lBaseFilePath} ${lTargetFileName} &>/dev/null
+
+  local lLinkFilePath=$(readlink ${lTargetFileName})
+
+  echo -e -n "          ${lBaseFilePath} => ${lTargetDirPath} [${lTargetFileName}]\r"
+  if [ "${lBaseFilePath}" != "${lLinkFilePath}" ]; then
+    echo -e "${DISP_ARG_NG}"
+    echo "    >> ${lLinkFilePath}"
+  else
+    echo -e "${DISP_ARG_OK}"
   fi
 }
 
@@ -58,6 +100,8 @@ echo
 
 LINUX_DISTRIBUTION=$(cat /etc/os-release | grep -E "^ID=" | sed -e "s/ID=//g")
 echo "$LINUX_DISTRIBUTION"
+LINUX_DISTRIBUTION_VER=$(cat /etc/os-release | grep -E "^VERSION_ID=" | sed -e "s/VERSION_ID=//g")
+echo "Ver.$LINUX_DISTRIBUTION_VER"
 
 ######################################################
 
@@ -67,16 +111,10 @@ echo "=== UPDATE PACKAGE MANAGER ==="
 echo "=============================="
 echo
 
-PACKAGE_MNG="DUMMY"
-cat /etc/os-release | grep -E "^ID=" | grep ubuntu
-if [ $? -eq 0 ]; then
-  PACKAGE_MNG="apt-get"
-fi
-
-if [ "$PACKAGE_MNG" != "DUMMY" ]; then
-  echo "" >~/setuplog.txt
-  sudo ${PACKAGE_MNG} -y update
-  sudo ${PACKAGE_MNG} -y upgrade
+echo "" >~/setuplog.txt
+if [ "${LINUX_DISTRIBUTION}" == "ubuntu" ]; then
+  sudo apt-get -y update
+  sudo apt-get -y upgrade
 else
   echo "Skip update"
 fi
@@ -89,61 +127,76 @@ echo "=== INSTALL SOFTWARE ==="
 echo "========================"
 echo
 
-InstallPackageByPackMng ${PACKAGE_MNG} git
-InstallPackageByPackMng ${PACKAGE_MNG} tig
-InstallPackageByPackMng ${PACKAGE_MNG} vim
-InstallPackageByPackMng ${PACKAGE_MNG} neovim
-InstallPackageByPackMng ${PACKAGE_MNG} shfmt
-InstallPackageByPackMng ${PACKAGE_MNG} gcc
-InstallPackageByPackMng ${PACKAGE_MNG} clang
-InstallPackageByPackMng ${PACKAGE_MNG} clang-format
-InstallPackageByPackMng ${PACKAGE_MNG} gdb
-InstallPackageByPackMng ${PACKAGE_MNG} cmake
-InstallPackageByPackMng ${PACKAGE_MNG} tmux
-InstallPackageByPackMng ${PACKAGE_MNG} peco
-InstallPackageByPackMng ${PACKAGE_MNG} nodejs
-InstallPackageByPackMng ${PACKAGE_MNG} npm
-InstallPackageByPackMng ${PACKAGE_MNG} node-js-beautify
-InstallPackageByPackMng ${PACKAGE_MNG} yarn
-InstallPackageByPackMng ${PACKAGE_MNG} python3
-InstallPackageByPackMng ${PACKAGE_MNG} python3-pip
-InstallPackageByPackMng ${PACKAGE_MNG} python3-autopep8
-InstallPackageByPackMng ${PACKAGE_MNG} docker
-InstallPackageByPackMng ${PACKAGE_MNG} docker-compose
-InstallPackageByPackMng ${PACKAGE_MNG} net-tools
-InstallPackageByPackMng ${PACKAGE_MNG} exuberant-ctags
-InstallPackageByPackMng ${PACKAGE_MNG} manpages-ja
-InstallPackageByPackMng ${PACKAGE_MNG} hugo
+if [ "${LINUX_DISTRIBUTION}" == "ubuntu" ]; then
+  InstallPackageByPackMng apt-get git
+  InstallPackageByPackMng apt-get tig
+  InstallPackageByPackMng apt-get vim
+  InstallPackageByPackMng apt-get neovim
+  InstallPackageByPackMng apt-get shfmt
+  InstallPackageByPackMng apt-get gcc
+  InstallPackageByPackMng apt-get clang
+  InstallPackageByPackMng apt-get clang-format
+  InstallPackageByPackMng apt-get gdb
+  InstallPackageByPackMng apt-get cmake
+  InstallPackageByPackMng apt-get tmux
+  InstallPackageByPackMng apt-get peco
+  InstallPackageByPackMng apt-get nodejs
+  InstallPackageByPackMng apt-get npm
+  InstallPackageByPackMng apt-get node-js-beautify
+  InstallPackageByPackMng apt-get yarn
+  InstallPackageByPackMng apt-get python3
+  InstallPackageByPackMng apt-get python3-pip
+  InstallPackageByPackMng apt-get python3-autopep8
+  InstallPackageByPackMng apt-get docker
+  InstallPackageByPackMng apt-get docker-compose
+  InstallPackageByPackMng apt-get net-tools
+  InstallPackageByPackMng apt-get exuberant-ctags
+  InstallPackageByPackMng apt-get hugo
+  InstallPackageByPackMng apt-get tree
+
+  InstallPackageByPackMng apt-get manpages-ja
+
+  sudo chmod +x /usr/share/doc/git/contrib/diff-highlight/diff-highlight
+else
+  echo "Skip install"
+fi
 
 ######################################################
 
-function CreateSymLinkAndCheck() {
-  # $1 <ex> /base/file/path/filename
-  local lBaseFilePath=$1
+echo
+echo "========================"
+echo "=== COMMAND CHECK ==="
+echo "========================"
+echo
 
-  # $2 <ex> /target/dir/path/
-  local lTargetDirPath=$2
+CommandCheck git
+CommandCheck tig
+CommandCheck vim
+CommandCheck nvim
+CommandCheck shfmt
+CommandCheck gcc
+CommandCheck clang
+CommandCheck clang-format
+CommandCheck gdb
+CommandCheck cmake
+CommandCheck tmux
+CommandCheck peco
+CommandCheck node
+CommandCheck npm
+CommandCheck js-beautify
+CommandCheck yarn
+CommandCheck python3
+CommandCheck python3-pip
+CommandCheck autopep8
+CommandCheck docker
+CommandCheck docker-compose
+CommandCheck ifconfig # net-tools
+CommandCheck ctags
+CommandCheck hugo
+CommandCheck tree
+CommandCheck diff-highlight
 
-  # $3 <ex> target_file_name
-  local lTargetFileName=$3
-
-  if [ "$lTargetFileName" == "./" ]; then
-    lTargetFileName=${lBaseFilePath##*/}
-  fi
-
-  CheckAndCdDir ${lTargetDirPath}
-  ln -s ${lBaseFilePath} ${lTargetFileName} &>/dev/null
-
-  local lLinkFilePath=$(readlink ${lTargetFileName})
-
-  echo -e -n "       ${lBaseFilePath} => ${lTargetDirPath} [${lTargetFileName}]\r"
-  if [ "${lBaseFilePath}" != "${lLinkFilePath}" ]; then
-    echo -e "\033[1;31m[ NG ]\033[0;39m"
-    echo "    >> ${lLinkFilePath}"
-  else
-    echo -e "\033[1;32m[ OK ]\033[0;39m"
-  fi
-}
+######################################################
 
 echo
 echo "===================="
@@ -189,7 +242,6 @@ echo
 CreateSymLinkAndCheck ${PWD_SCRIPT}/dotfiles/.gitconfig ~/ ./
 CreateSymLinkAndCheck ${PWD_SCRIPT}/dotfiles/.git-prompt.sh ~/ ./
 CreateSymLinkAndCheck ${PWD_SCRIPT}/dotfiles/.tigrc ~/ ./
-sudo chmod +x /usr/share/doc/git/contrib/diff-highlight/diff-highlight
 
 ######################################################
 
@@ -206,5 +258,11 @@ CreateSymLinkAndCheck ${PWD_SCRIPT}/dotfiles/.tmux.conf ~/ ./
 ######################################################
 
 cd ${PWD_SCRIPT}
+
+DISP_ARG_OK=
+DISP_ARG_NG=
+PWD_SCRIPT=
+LINUX_DISTRIBUTION=
+LINUX_DISTRIBUTION_VER=
 
 # end
